8e501a9abd9475ce3ede224b9df8d08539402d26
fnateghi <56291706+fnateghi@users.noreply.github.com>
2021-03-04 16:30:41 +0100

    Struktur angepasst
    Struktur angepasst
    Format angepasst
    
    Pagination zeit immer noch mehrere 100 Seiten, wenn du fixen kannst, gerne.

--- datenseite.php
+++ datenseite.php
@@ -13,7 +13,6 @@
                  <th>Name</th>
                  <th>VPE</th>
              </tr>
-
          </thead>
          <tbody>
              <!-- 
@@ -22,29 +21,11 @@
 ###############################################################
 -->
              <?php 
-             $con = mysqli_connect("localhost", "root", "", "db_händler");
-             if(!$con)
-             {
-                 echo ' Please Check Your Connection ';
-             }
-             if(isset($_GET['page']))
-                {
-                    $page = $_GET['page'];
-                }
-                else
-                {
-                    $page = 1;
-                }
-
-             $num_per_page = 20;
-            $start_from = ($page-1)*20;
-             $query = "SELECT * FROM tb_artikel LIMIT $start_from,$num_per_page";
-             $result = mysqli_query($con,$query);
-
-        //echo var_dump($xml);
-        $sql = "SELECT * FROM tb_artikel";
-        while($row = mysqli_fetch_assoc($result)) {
-        ?>
+    INCLUDE_ONCE("pagination.php");
+    //echo var_dump($xml);
+    $sql = "SELECT * FROM tb_artikel";
+    while($row = mysqli_fetch_assoc($result)) {
+    ?>
              <tr>
                  <div>
                      <?php
@@ -59,6 +40,7 @@
              </tr>
          </tbody>
      </table>
+     <br>
      <?php
      $pr_query = "select * from tb_artikel";
      $pr_result = mysqli_query($con,$pr_query);
@@ -80,30 +62,4 @@
      ?>
      <?php INCLUDE_ONCE("tpl/footer.php") ?>
  </body>
-
  </html>
-
- <!--
-            // $mysqli = NEW mysqli("localhost", "root", "", "db_händler");
-            // $rpp = 10;
-
-             //seite überprüfen
-             isset($_GET['page']) ? $page = $_GET['page'] : $page = 0;
-             
-             // Steite 1 überprüfen
-             if($page > 1){
-                 $start = ($page * $rpp) - $rpp;
-             }else{
-                 $start = 0;
-             }
-             $start_from = ($page-1)*05;
-             // Querry DB für die ganze Einträge
-             $resultSet = $mysqli->query("SELECT id FROM tb_artikel");
-
-             // Allte Einträge einholen
-             $numRows = $resultSet->num_rows;
-             // alle Seiten anzeigen
-             $totalPages = $numRows / $rpp;
-             // Querry Results
-             $resultSet = $mysqli->query("SELECT * FROM tb_artikel LIMIT $start_from, $rpp");
-     -->
--- pagination.php
+++ pagination.php
@@ -1,9 +1,19 @@
 <?php
-// Connention zu der DB aufbauen
-$conn = mysqli_connect("localhost", "root", "", "db_händler");
-// Wie viele Einträge pro Seite
-
-
-
-
+$con = mysqli_connect("localhost", "root", "", "db_händler");
+if (!$con)
+{
+    echo ' Please Check Your Connection ';
+}
+if (isset($_GET['page']))
+{
+    $page = $_GET['page'];
+}
+else
+{
+    $page = 1;
+}
+$num_per_page = 20;
+$start_from = ($page - 1) * 20;
+$query = "SELECT * FROM tb_artikel LIMIT $start_from,$num_per_page";
+$result = mysqli_query($con, $query);
 ?>
