f2c07e9b039394e93379fb445f92179a691784a3
YDamm <70517379+YDamm@users.noreply.github.com>
2021-03-27 16:56:46 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -4,24 +4,8 @@
     <!-- Standard definierte Navigationsmenü Template aufrufen -->
     <?php INCLUDE_ONCE("tpl/nav.php") ?>
     <br>
-    <h1 style="text-align:center"> digitale OBST und GEMUESE  </h1>
+    <h1 style="text-align:center"> Digitaler Obst und Gemuese Shop </h1>
     <div class="container ">
-        <div class="row" style="text-align:center">
-                <h1 class="white">Datenbank erstellen</h1>
-        </div>
-        <!--  -->
-        <div class="row" style="justify-content:center">
-            <div class="col-md-6 col-md-offset-3">
-                <div class="btn-container center">
-                    <br>
-                    <p id="namefile">Beim Klicken auf dem Button, wird die Datenbank angelegt</p>
-                    <br>
-                    <!-- Datenbanktabellen Skript zum Erzeugen aufrufen -->
-                    <button class="btn btn-primary"><a href="db/db.php" target="_blank"
-                            style="color:white;">erstellen</a></button>
-                </div>
-            </div>
-        </div>
         <!-- upload Formulare -->
         <div class="container" style="text-align:center" >
                     <h1 class="white" >Datei hochladen</h1>
@@ -31,7 +15,7 @@
                     <div class="col-md-6 col-md-offset-3 ">
                         <div class="btn-container " style="text-align:center">
                             <br>
-                            <p id="namefile">Nur XML Datei ist erlaubt (.xml) - Bitte Datei als artikeldaten.xml benennen.</p>
+                            <h4 id="namefile">Nur XML Datei ist erlaubt (.xml)! <br>Bitte Datei als artikeldaten.xml benennen.</h4>
                             <input type="file" name="fileToUpload" id="fileToUpload">
                             <br>
                             <br>
@@ -44,8 +28,23 @@
                         </div>
                     </div>
                 </div>
-
             </form>
+	<div class="row" style="text-align:center">
+        	<h1 class="white">Datenbank erstellen</h1>
+        </div>
+        <!--  -->
+        <div class="row" style="justify-content:center">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="btn-container center">
+                    <br>
+                    <h4 id="namefile">Beim Klicken auf dem Button, wird die Datenbank angelegt</h4>
+                    <br>
+                    <!-- Datenbanktabellen Skript zum Erzeugen aufrufen -->
+                    <button class="btn btn-primary"><a href="db/db.php" target="_blank"
+                            style="color:white;">erstellen</a></button>
+                </div>
+            </div>
+        </div>
         </div>
         <!-- Standard definierte Footer Template aufrufen -->
         <?php INCLUDE_ONCE("tpl/footer.php") ?>
--- pagination.php
+++ pagination.php
@@ -1,11 +1,11 @@
 <?php
-// Verbindung zur Datenbank
+// Datenbank anbindung
 $con = mysqli_connect("localhost", "ghaendler_root", "root", "ghaendler");
 if (!$con)
 {
     echo ' Bitte noch einmal überprüfen ';
 }
-// Daten per GET-Abfrage von der Seite holen
+// Daten per Get Abfrage von der Seite holen
 if (isset($_GET['page']))
 {
     $page = $_GET['page'];
@@ -14,9 +14,9 @@ else
 {
     $page = 1;
 }
-// legt Artikelanzahl pro Seite fest
-$num_per_page = 100;
-$start_from = ($page - 1) * 100;
+// Artikelanzahl pro Seite 
+$num_per_page = 250;
+$start_from = ($page - 1) * 250;
 $query = "SELECT * FROM tb_artikel LIMIT $start_from,$num_per_page";
 $result = mysqli_query($con, $query);
 ?>
